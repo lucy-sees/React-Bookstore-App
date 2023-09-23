576d85d3c5a32dd7ce5ae6ceb81e8453e8f8c3fa
Lucy W. Mwangi <100368317+lucy-sees@users.noreply.github.com>
2023-09-23 23:17:49 +0300

    Set up components for each app functionality

---
+++ src/components/Categories.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/Categories.css';
+
+const Categories = () => (
+  <section className="categories-container">
+    <button type="button" className="check-btn">
+      Check Status
+    </button>
+  </section>
+);
+
+export default Categories;
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import '../styles/Navigation.css';
+import { BsPersonBoundingBox } from 'react-icons/bs';
+
+const Navigation = () => (
+  <nav className="navmenu-desktop">
+    <a href="/" rel="noreferrer" className="logo">
+      Bookstore
+    </a>
+    <ul className="nav-items">
+      <li className="nav-item">
+        <NavLink
+          to="/"
+          end
+          className={({ isActive }) => (isActive ? 'activeLink' : undefined)}
+        >
+          Home
+        </NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink
+          to="categories"
+          className={({ isActive }) => (isActive ? 'activeLink' : undefined)}
+        >
+          Categories
+        </NavLink>
+      </li>
+    </ul>
+    <BsPersonBoundingBox className="user" />
+  </nav>
+);
+
+export default Navigation;
